329a8e00c6fbcd3ff0c4a21066249c71e22b627f
Brian Muranja <brianwaweru58@gmail.com>
2025-06-09 13:58:55 +0200

    feat: filter option multiple and single.

--- bachelor-app/src/components/skeleton-loading.tsx
+++ bachelor-app/src/components/skeleton-loading.tsx
@@ -3,29 +3,10 @@
 import { useMediaQuery } from "@/hooks/use-media-query";
 
 export function FieldsSkeleton() {
-  const isDesktop = useMediaQuery("(min-width: 1024px)");
-  const isTablet = useMediaQuery("(min-width: 769px) and (max-width: 1024px)");
-  const isMobile = useMediaQuery("(max-width: 768px)");
-
-  // Determine number of skeletons based on screen size
-  const skeletonCount = isDesktop ? 12 : isTablet ? 9 : 6;
-
   return (
-    <div
-      className={`${
-        isMobile ? "grid grid-cols-2 gap-4 justify-center px-9" : "fields-grid"
-      }`}
-    >
-      {Array.from({ length: skeletonCount }).map((_, i) => (
-        <div
-          key={i}
-          className={`skeleton ${
-            isMobile
-              ? "w-[174px] h-[174px] rounded-[15px] mx-auto"
-              : "field-card"
-          }`}
-          style={{ opacity: 0.5 }}
-        ></div>
+    <div className="fields-grid-skeleton">
+      {Array.from({ length: 12 }).map((_, i) => (
+        <div key={i} className="skeleton field-card-skeleton"></div>
       ))}
     </div>
   );
