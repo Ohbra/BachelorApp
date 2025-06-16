827f64fc859f083d9702816306c877cc25d16d12
Brian Muranja <brianwaweru58@gmail.com>
2025-06-16 15:28:30 +0200

    feat(deployment) issues 6

--- bachelor-app/eslint.config.mjs
+++ bachelor-app/eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "src/app/backend/generated/**/*",
+      "**/*.generated.*",
+      ".next/**/*",
+      "node_modules/**/*"
+    ]
+  }
 ];
 
 export default eslintConfig;
